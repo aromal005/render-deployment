421a3fb8132e01bfa7c7bd03f1120e9be53e25dc
Aromal-005 <aromal.v@gksinfotech.com>
2025-07-01 12:00:19 +0530

    moved secret key into .env

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k*+@nh*(%-$2@piyrz_z=jp8wdi_er3%tl@!sw**s+$4#s311d'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
