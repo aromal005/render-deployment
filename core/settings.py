be840e1cc258e437c30086311846ce17a660dee6
Aromal-005 <aromal.v@gksinfotech.com>
2025-07-01 12:32:40 +0530

    added live link of onrender

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(",")
+ALLOWED_HOSTS = ["127.0.0.1:8000","render-deployment-5826.onrender.com"]
 
 
 # Application definition
